523b4e1ac46f20e374532b9fba9e728a7afc2569
CLendering <camile.lendering@gmail.com>
2024-05-13 18:21:45 +0200

    To HDF5

--- main.py
+++ main.py
@@ -6,69 +6,21 @@ import urllib
 from fuzzywuzzy import process
 import pandas as pd
 from collections import defaultdict
-
-PARKING_VIOLATIONS_2024 = (
-    "https://data.cityofnewyork.us/resource/pvqr-7yc4.csv?$limit=13000000"
-)
-PARKING_VIOLATIONS_2023 = (
-    "https://data.cityofnewyork.us/api/views/869v-vr48/rows.csv?accessType=DOWNLOAD"
-)
-PARKING_VIOLATIONS_2022 = (
-    "https://data.cityofnewyork.us/api/views/7mxj-7a6y/rows.csv?accessType=DOWNLOAD"
-)
-PARKING_VIOLATIONS_2020 = (
-    "https://data.cityofnewyork.us/api/views/p7t3-5i9s/rows.csv?accessType=DOWNLOAD"
-)
-PARKING_VIOLATIONS_2019 = (
-    "https://data.cityofnewyork.us/api/views/faiq-9dfq/rows.csv?accessType=DOWNLOAD"
-)
-PARKING_VIOLATIONS_2018 = (
-    "https://data.cityofnewyork.us/api/views/a5td-mswe/rows.csv?accessType=DOWNLOAD"
-)
-PARKING_VIOLATIONS_2017 = (
-    "https://data.cityofnewyork.us/api/views/2bnn-yakx/rows.csv?accessType=DOWNLOAD"
-)
-PARKING_VIOLATIONS_2016 = (
-    "https://data.cityofnewyork.us/api/views/kiv2-tbus/rows.csv?accessType=DOWNLOAD"
-)
-PARKING_VIOLATIONS_2015 = (
-    "https://data.cityofnewyork.us/api/views/c284-tqph/rows.csv?accessType=DOWNLOAD"
-)
-PARKING_VIOLATIONS_2014 = (
-    "https://data.cityofnewyork.us/api/views/jt7v-77mi/rows.csv?accessType=DOWNLOAD"
-)
-
-parking_violations = [
-    PARKING_VIOLATIONS_2024,
-    PARKING_VIOLATIONS_2023,
-    PARKING_VIOLATIONS_2022,
-    PARKING_VIOLATIONS_2020,
-    PARKING_VIOLATIONS_2019,
-    PARKING_VIOLATIONS_2018,
-    PARKING_VIOLATIONS_2017,
-    PARKING_VIOLATIONS_2016,
-    PARKING_VIOLATIONS_2015,
-    PARKING_VIOLATIONS_2014,
-]
-
+import vaex
 
 def download_data(data_folder="data"):
-    """
-    Downloads parking violations data from a specified URL and saves it to the given data path.
+    """""
+    Download the data from the NYC Open Data website.
 
     Args:
-        data_folder (str): The folder where the downloaded data will be saved. Defaults to "data".
+        data_folder (str): The folder where the data will be downloaded. Defaults to "data".
 
     Returns:
         None
-    """
-    year = 2024
-    for url in parking_violations:
-        file_name = f"parking_violations_{year}.csv"
-        file_path = os.path.join(data_folder, file_name)
-        if not os.path.exists(file_path):
-            urllib.request.urlretrieve(url, file_path)
-        year -= 1
+    """""
+    # run data/nyt.sh script to download the data
+    os.system(f"sh {data_folder}/nyt.sh")
+    
 
 
 class Task1:
@@ -106,7 +58,7 @@ class Task1:
             memory_limit="6.5GB",
             local_directory="/tmp",
         )
-        print(client.scheduler_info()["services"])
+        print(client.scheduler_info()["services"]) 
         return client
 
     def read_data(
@@ -124,49 +76,22 @@ class Task1:
         Returns:
             None
         """
+
         dtype_spec = {
-            "summons_number": "int64",
-            "plate_id": "str",
-            "registration_state": "str",
-            "plate_type": "str",
-            "violation_code": "int64",
-            "vehicle_body_type": "str",
-            "vehicle_make": "str",
-            "issuing_agency": "str",
-            "street_code1": "int64",
-            "street_code2": "int64",
-            "street_code3": "int64",
-            "vehicle_expiration_date": "int64",
-            "violation_location": "str",
-            "violation_precinct": "int64",
-            "issuer_precinct": "int64",
-            "issuer_code": "int64",
-            "issuer_command": "str",
-            "issuer_squad": "str",
-            "violation_time": "str",
-            "time_first_observed": "str",
-            "violation_county": "str",
-            "violation_in_front_of_or_opposite": "str",
-            "house_number": "str",
-            "street_name": "str",
-            "intersecting_street": "str",
-            "date_first_observed": "int64",
-            "law_section": "int64",
-            "sub_division": "str",
-            "violation_legal_code": "str",
-            "days_parking_in_effect": "str",
-            "from_hours_in_effect": "str",
-            "to_hours_in_effect": "str",
-            "vehicle_color": "str",
-            "unregistered_vehicle": "str",
-            "vehicle_year": "int64",
-            "meter_number": "str",
-            "feet_from_curb": "int64",
-            "violation_post_code": "str",
-            "violation_description": "str",
-            "no_standing_or_stopping_violation": "str",
-            "hydrant_violation": "str",
-            "double_parking_violation": "str",
+            "summons_number": "int64", "plate_id": "str", "registration_state": "str", "plate_type": "str",
+            "violation_code": "int64", "vehicle_body_type": "str", "vehicle_make": "str", "issuing_agency": "str",
+            "street_code1": "int64", "street_code2": "int64", "street_code3": "int64",
+            "vehicle_expiration_date": "int64", "violation_location": "str", "violation_precinct": "int64",
+            "issuer_precinct": "int64", "issuer_code": "int64", "issuer_command": "str", "issuer_squad": "str",
+            "violation_time": "str", "time_first_observed": "str", "violation_county": "str",
+            "violation_in_front_of_or_opposite": "str", "house_number": "str", "street_name": "str",
+            "intersecting_street": "str", "date_first_observed": "int64", "law_section": "int64",
+            "sub_division": "str", "violation_legal_code": "str", "days_parking_in_effect": "str",
+            "from_hours_in_effect": "str", "to_hours_in_effect": "str", "vehicle_color": "str",
+            "unregistered_vehicle": "str", "vehicle_year": "int64", "meter_number": "str",
+            "feet_from_curb": "int64", "violation_post_code": "str", "violation_description": "str",
+            "no_standing_or_stopping_violation": "str", "hydrant_violation": "str",
+            "double_parking_violation": "str"
         }
 
         if not os.path.exists(pq_file):
@@ -176,7 +101,7 @@ class Task1:
             df = dd.read_csv(self.data_path, dtype=dtype_spec)
             df.to_hdf(hdf_file, key="data", min_itemsize={"values_block_1": 100})
 
-    def run(
+    def get_file_sizes(
         self,
         pq_file="parking_violations_2024.parquet",
         hdf_file="parking_violations_2024.h5",
@@ -251,8 +176,13 @@ def read_and_process_file(file, reference_cols, base_path='data/', temp_dir='tem
 
     return temp_file
 
-def process_all_files(directory='data/', reference_file='parking_violations_2024.csv', temp_dir='temp/'):
-    client = Client()
+def process_all_files(directory='data/', reference_file='2024.csv', temp_dir='temp/'):
+    client = Client(
+            n_workers=2,
+            threads_per_worker=2,
+            memory_limit="6.5GB",
+            local_directory="/tmp",
+        )
     reference_path = os.path.join(directory, reference_file)
     reference_df = pd.read_csv(reference_path, nrows=0)
     reference_cols = list(reference_df.columns)
@@ -269,13 +199,32 @@ def process_all_files(directory='data/', reference_file='parking_violations_2024
         print("No dataframes to concatenate.")
 
     client.close()
+
+def csv_to_hdf5():
+    """
+    Convert a CSV file to Parquet format.
+    """
+
+    client = Client(
+            n_workers=2,
+            threads_per_worker=2,
+            memory_limit="4.5GB",
+            local_directory="/tmp",
+        )
+    print(client.scheduler_info()["services"])
+    df = dd.read_csv("data/*.csv", dtype="object")
+    df = df.repartition(npartitions=10000)
+    df.to_hdf("parking_violations.hdf5", key="data", mode="w", min_itemsize={'Violation Description': 100, 'Plate ID': 50})
+    client.close()
+
+
 if __name__ == "__main__":
     # data_path = "data/parking_violations_2024.csv"
     # task1 = Task1(data_path)
     # print(task1.run())
     #download_data()
     # Create a mapping based on the 2024 file as the standard
-    process_all_files()
-
+    #process_all_files(
+    csv_to_hdf5()
     
     #load_all_in_one_parquet()
